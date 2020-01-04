3035819d2e1e80fc0e781e88d2c02bc5fc019049
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-04 14:19:11 +0600

    Updates

---
+++ precache-manifest.89189e315a851bb8b9d9f6733bc2dc94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd7ba1336f19fe84a37f2301cc2bf9f",
+    "url": "/task-manager-react/index.html"
+  },
+  {
+    "revision": "2d6afd34cbf9793b74e9",
+    "url": "/task-manager-react/static/css/main.e46c84e9.chunk.css"
+  },
+  {
+    "revision": "113d566453fc39f122ee",
+    "url": "/task-manager-react/static/js/2.b7befe2d.chunk.js"
+  },
+  {
+    "revision": "b846902a03bddff09b754d87bdd08a3c",
+    "url": "/task-manager-react/static/js/2.b7befe2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d6afd34cbf9793b74e9",
+    "url": "/task-manager-react/static/js/main.323f9ea8.chunk.js"
+  },
+  {
+    "revision": "d90626c8aff826115b78",
+    "url": "/task-manager-react/static/js/runtime-main.6c1bf770.js"
+  }
+]);
